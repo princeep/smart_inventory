48f656ec6ae3d27c55bd5c70cea7c1edec352c5e
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-13 22:58:20 +0530

    feat: add Product model class with constructors

--- src/main/java/com/smarthub/model/Product.java
+++ src/main/java/com/smarthub/model/Product.java
@@ -1,4 +1,20 @@
 package main.java.com.smarthub.model;
 
 public class Product {
+    private  int id;
+    private  String name;
+    private  String category;
+    private  double price;
+    private  int quantity;
+
+    public Product(){
+    };
+
+    public Product(String name,String category,double price,int quantity){
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
 }
