a12bbef7d399fe40c67ff0a365f678db5f76f413
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-14 00:56:01 +0530

    feat(dao): add ProductDAO interface

--- src/main/java/com/smarthub/dao/ProductDAO.java
+++ src/main/java/com/smarthub/dao/ProductDAO.java
@@ -1,4 +1,12 @@
 package main.java.com.smarthub.dao;
+import main.java.com.smarthub.model.Product;
 
-public class ProductDAO {
+public interface ProductDAO {
+    void addProduct(Product product);
+
+    void viewProducts();
+
+    void updateProduct(Product product);
+
+    void deleteProduct(int id);
 }
