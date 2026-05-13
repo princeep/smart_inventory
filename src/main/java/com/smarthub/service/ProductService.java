c9b50f5585aefadaf52cda0627a5237a59f7c2ea
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-14 02:54:18 +0530

    feat(service): create ProductService class

--- src/main/java/com/smarthub/service/ProductService.java
+++ src/main/java/com/smarthub/service/ProductService.java
@@ -1,4 +1,6 @@
 package main.java.com.smarthub.service;
+import main.java.com.smarthub.dao.ProductDAOImpl;
 
 public class ProductService {
+    public static ProductDAOImpl dao = new ProductDAOImpl();
 }
