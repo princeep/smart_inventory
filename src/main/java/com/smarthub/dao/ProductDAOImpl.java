9681975ee60bbb83b592dd9b0f5adc4fd6708326
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-14 02:15:52 +0530

    feat(product): add database insertion logic for products

--- src/main/java/com/smarthub/dao/ProductDAOImpl.java
+++ src/main/java/com/smarthub/dao/ProductDAOImpl.java
@@ -1,4 +1,28 @@
 package main.java.com.smarthub.dao;
+import main.java.com.smarthub.config.DBConnection;
+import main.java.com.smarthub.model.Product;
+import  java.sql.*;
+public class ProductDAOImpl implements ProductDAO {
+    @Override
+    public void addProduct(Product product){
+        String sql = "insert into products(name,category,price,quantity) values(?,?,?,?)";
+        try{
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
 
-public class ProductDAOImpl {
+            ps.setString(1,product.getName());
+
+            ps.setString(2, product.getCategory());
+
+            ps.setDouble(3,product.getPrice());
+
+            ps.setInt(4,product.getQuantity());
+
+            ps.executeUpdate();
+            System.out.println("Product added");
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
